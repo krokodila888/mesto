298580d52ed064917ffaffe51b59181a34521403
Evgenia <101995565+krokodila888@users.noreply.github.com>
2022-06-04 23:18:10 +0300

    Now it can add new cards with working buttons

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,7 @@ const jobInput = document.querySelector('.popup__input_status');
 const nameText = document.querySelector('.profile__username');
 const statusText = document.querySelector('.profile__status');
 const addButton = document.querySelector('.profile__add-button');
+const newCard = document.querySelector('.element');
 
 const initialCards = [
   {
@@ -37,8 +38,26 @@ const initialCards = [
   }
 ]; 
 
+initialCards.forEach(function (element) {
+  const templateBlock = document.querySelector('.template-element').content;
+  const cardsHolder1 = document.querySelector('.elements');
+  const blockToClone = templateBlock.querySelector('.element').cloneNode(true);
+  blockToClone.querySelector('.element__text').textContent = element.name; 
+  blockToClone.querySelector('.element__photo').src = element.link;
+  cardsHolder1.prepend(blockToClone);});
+
 function editProfile() {popupProfile.classList.add('popup_opened')};
 editButton.addEventListener('click', editProfile); 
+
+
+const cardRemoveButtons = document.querySelectorAll('.element__remove-button');
+for (const button of cardRemoveButtons) {
+  button.addEventListener('click', () => button.closest('.element').remove());
+}
+
+const likeButtons = document.querySelectorAll('.element__like-button');
+for (const button of likeButtons) {button.addEventListener ('click', function (e) {e.target.classList.toggle('element__like-button_active')})};
+
 
 let closeButtons = document.querySelectorAll('.popup__close-button');
 console.log(closeButtons);
@@ -46,27 +65,69 @@ for (const button of closeButtons) {
 button.addEventListener('click', () => button.closest('.popup').classList.remove('popup_opened'));
 }
 
-function closeProfile() {popupProfile.classList.remove('popup_opened');
+
+
+function closeProfilePopup() {popupProfile.classList.remove('popup_opened');
 nameInput.value = nameText.textContent;
 jobInput.value = statusText.textContent;
 }
-closeButton.addEventListener('click', closeProfile); 
+closeButton.addEventListener('click', closeProfilePopup); 
 
-let formElement = document.querySelector('.popup__save-button');
+const profileSaveButton = document.querySelector('.popup__profile-save-button');
+profileSaveButton.onclick = function (evt) {
+  evt.preventDefault();
+  nameText.textContent = nameInput.value;
+  statusText.textContent = jobInput.value;
+  closeProfilePopup();
+};
+  
 
-formElement.onclick = function (evt) {
-    evt.preventDefault();
-    nameText.textContent = nameInput.value;
-    statusText.textContent = jobInput.value;
-    closeProfile();
-  };
-
- let likeButtons = document.querySelectorAll('.element__like-button');
-  for (const button of likeButtons) {
-  button.addEventListener('click', () => button.classList.toggle('element__like-button_active'));
-}
 
 function editMesto() {popupMesto.classList.add('popup_opened')};
 addButton.addEventListener('click', editMesto); 
 
+const renderCardButton = document.querySelector('.popup_add-card-button');
+const cardName = document.querySelector('.element__text');
+const cardPhoto = document.querySelector('.element__photo');
+const closeButtonMesto = document.querySelector('.popup__mesto-close-button');
+const photoLinkInput = document.querySelector('.popup__input_photo-link');
+const photoNameInput = document.querySelector('.popup__input_photo-name');
+
+function closeMestoPopup() {
+  popupMesto.classList.remove('popup_opened');
+  photoNameInput.value = "";
+  photoLinkInput.value = "";
+}
+closeButtonMesto.addEventListener('click', closeMestoPopup); 
+
+
+function addCard(photoLinkInput, photoNameInput) {
+  const templateBlock = document.querySelector('.template-element').content;
+  const cardsHolder = document.querySelector('.elements');
+  const blockToClone = templateBlock.querySelector('.element').cloneNode(true);
+  blockToClone.querySelector('.element__like-button').addEventListener('click', function (evt) {evt.target.classList.toggle('element__like-button_active');}); 
+  blockToClone.querySelector('.element__remove-button').addEventListener('click', function (evt) {evt.target.closest('.element').remove();}); 
+   
+  blockToClone.querySelector('.element__text').textContent = photoNameInput;
+  blockToClone.querySelector('.element__photo').setAttribute('src', photoLinkInput);
+  cardsHolder.prepend(blockToClone);
+}
+
+  
+renderCardButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  const photoLinkInput = document.querySelector('.popup__input_photo-link');
+  const photoNameInput = document.querySelector('.popup__input_photo-name');
+  addCard(photoLinkInput.value, photoNameInput.value);
+     
+  photoLinkInput.value = '';
+  photoNameInput.value = '';
+  closeMestoPopup()
+});
+  for (const button of cardRemoveButtons) {
+  button.addEventListener('click', () => button.closest('.element').remove());
+};
+
+
+ 
 
