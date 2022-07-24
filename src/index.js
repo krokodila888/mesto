d1f03a35a7130bb0584dc8c4ced0b48869c5a01f
Evgenia <101995565+krokodila888@users.noreply.github.com>
2022-07-25 01:12:42 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Section } from './components/Section.js';
 import { PopupWithImage } from './components/PopupWithImage.js';
 import { PopupWithForm } from './components/PopupWithForm.js';
 import { UserInfo } from './components/UserInfo.js';
-import '../pages/index.css';
+import './pages/index.css';
 
 const enableValidationClasses = ({
   formSelector: '.popup__form-container',
